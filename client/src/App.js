6fdfb83470e3784349d5680ae725556138a96ad1
VlaNit <vova.komov37@gmail.com>
2025-05-18 03:17:55 +0300

    Визуал редактора заказа работает

--- client/src/App.js
+++ client/src/App.js
@@ -33,6 +33,7 @@ const App = observer((props) => {
   const [is_register_open, setRegister] = useState(false);
 
   document.body.style.margin = 0;
+  const current_width = window.innerWidth;
 
   useEffect(() => {
     shopping_cart_store.getDataFromSessionStorage();
@@ -45,7 +46,7 @@ const App = observer((props) => {
   }, []);
 
   return (
-    <div style={{...interface_styles.body, backgroundColor: interface_colors.background_color, color: interface_colors.text_color, width: "1530px"}}>
+    <div style={{...interface_styles.body, backgroundColor: interface_colors.background_color, color: interface_colors.text_color, width: current_width}}>
       <div style={is_page_darkened?interface_styles.darkened:null}>
         <Header></Header>
         <NavPanel shopping={setShoppingCart} login={setLogin} darken={setDark}></NavPanel>
